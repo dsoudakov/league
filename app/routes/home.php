b4378e776da9a7615599f0c11f57a939ee72b15f
DS <dsoudakov@gmail.com>
2017-03-16 16:12:03 -0400

    update offset for server time in mysql

--- app/routes/home.php
+++ app/routes/home.php
@@ -28,3 +28,23 @@ $app->get('/aboutclub', function ($request,$response,$args) use ($app)
 	return $response;
 
 })->setName('about.club');
+
+$app->get('/abouttest', function ($request,$response,$args) use ($app)
+{
+		$hour_offset_mysql = '+ INTERVAL 3 HOUR';
+
+        $sql = 'SELECT 
+                    concat(u.first_name, \' \', u.last_name, \' (\', u.email, \')\') as email 
+                    FROM usersactive ua 
+                    LEFT JOIN users u on u.id = ua.user_id 
+                    WHERE last_active > (NOW() '. $hour_offset_mysql  .' - INTERVAL 15 MINUTE)';
+
+        $ua = R::getAll($sql);
+
+        foreach ($ua as $v) {
+            $out .= $v['email'] . BR;
+        }
+
+        var_dump($out);		
+
+})->setName('about.test');
